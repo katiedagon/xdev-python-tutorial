000d78aba486e03e973158a549e62401fe0da10a
Katie Dagon <katie.dagon@gmail.com>
2021-02-10 17:18:47 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Katie Dagon",
+    author_email="kdagon@ucar.edu",
+    packages=["mysci"],
+    install_requires=[],
+)
